348619335b71874e456bcdf3dbc94738cb48d273
Krzysztof Wiśniewski <krzysiek@filmaster.pl>
2013-11-13 17:58:33 +0100

    added class for serializing cassandra models to json

--- film_common/utils/json_tools.py
+++ film_common/utils/json_tools.py
@@ -58,4 +58,16 @@ def loads(s):
     return json.loads(s, object_hook=object_hook)
 
 
+class SerializableCassandraModel():
+
+    """
+        interface for cqlengine models to become json serializable
+    """
+    def __getstate__(self):
+        return dict((k, getattr(self, k)) for k in self._columns.keys() if getattr(self, k) is not None)
+
+    def __jsonhint__(self):
+        return self.__class__, [], self.__getstate__()
+
+
 
