4fda98219da8b040a1d7634ae4c1b531b65f3d7f
Krzysztof Wiśniewski <krzysiek@filmaster.pl>
2013-11-21 14:44:29 +0100

    added missing methods

--- film_common/utils/fb/proxy_api.py
+++ film_common/utils/fb/proxy_api.py
@@ -48,6 +48,15 @@ class ProxyAPI(object):
     def get_recent_likes(self, hash_id):
         return self.fetch_with_pagination('/' + hash_id + '/likes', fields='id,name,category,created_time', limit=ProxyAPI.RECENT_LIKES_PAGES)
 
+    def get_watched_movies(self, hash_id):
+        return self.fetch_with_pagination('/' + hash_id + '/video.watches')
+
+    def get_wants_to_watch(self, hash_id):
+        return self.fetch_with_pagination('/' + hash_id + '/video.wants_to_watch')
+
+    def get_movies(self, hash_id):
+        return self.fetch_with_pagination('/' + hash_id + '/movies')
+
     def get_object_by_id(self, hash_id, fields=None):
         return self.api.get('/' + hash_id)
 
