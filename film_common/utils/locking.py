8a43fc2e230e3e92a934b350900db5105346c3ca
Mariusz Kryński <mrk@sed.pl>
2013-11-22 11:39:23 +0000

    film_common.utils.locking fix

--- film_common/utils/locking.py
+++ film_common/utils/locking.py
@@ -1,4 +1,5 @@
 import functools
+from django.conf import settings
 
 from film_common.utils.redis_intf import acquire_lock
 import logging
@@ -8,7 +9,7 @@ def single_instance_task(func):
     @functools.wraps(func)
     def wrapper(*args, **kw):
         try:
-            with acquire_lock("single_instance_task_%s_%r_%r" % (func.__name__, args, kw)):
+            with acquire_lock("single_instance_task_%s_%s_%r_%r" % (settings.SECRET_KEY, func.__name__, args, kw)):
                 func(*args, **kw)
         except acquire_lock.AlreadyAcquired, e:
             logging.warning("lock for %r already acquired" % func.__name__)
